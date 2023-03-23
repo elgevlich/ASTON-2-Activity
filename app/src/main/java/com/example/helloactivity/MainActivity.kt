2bc599c3f7b78f9ae410ff22d5b236d970918151
elgevlich <elgevlich@mail.ru>
2023-03-23 11:00:05 +0300

    Initial commit

--- app/src/main/java/com/example/helloactivity/MainActivity.kt
+++ app/src/main/java/com/example/helloactivity/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.helloactivity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,15 +10,14 @@ class MainActivity : AppCompatActivity() {
 	private var count = 0
 	private lateinit var countView: TextView
 
-	companion object {
-
-		const val CURRENT_COUNT = "current_count"
-	}
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		countView = findViewById(R.id.show_count)
+
+		if (savedInstanceState != null) {
+			countView.text = savedInstanceState.getString("current_count")
+		}
 	}
 
 	override fun onSaveInstanceState(savedInstanceState: Bundle) {
@@ -27,21 +25,9 @@ class MainActivity : AppCompatActivity() {
 		savedInstanceState.putString("current_count", countView.text.toString())
 	}
 
-	override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-		super.onRestoreInstanceState(savedInstanceState)
-		countView.text = savedInstanceState.getString("current_count")
-	}
-
 
 	fun countUp(view: View) {
 		count++
 		countView.text = count.toString()
 	}
-
-	fun launchSecondActivity(view: View) {
-		val intent = Intent(this, SecondActivity::class.java)
-		intent.putExtra(CURRENT_COUNT, count.toString())
-		startActivity(intent)
-	}
-
 }
